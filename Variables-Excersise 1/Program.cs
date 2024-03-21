2cdf0b0667a8d3b9ccc84dde53164d58c42d5b6d
Dalton Bate <dilton8763@gmail.com>
2024-03-21 13:53:49 -0400

    Completed C# Variables work

---
+++ Variables-Excersise 1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Declare and initialize variables
+String name = "Frank";
+int age = 38;
+char middleInitial = 'F';
+bool isOver18 = true;
+double currentTemp = 82.9;
+decimal currentPrice = 29.99m;
+// Writing the variables to the console
+Console.WriteLine($"Name: {name}");
+Console.WriteLine($"Age: {age}");
+Console.WriteLine($"Middle Initial: {middleInitial}");
+Console.WriteLine($"Is Over 18: {isOver18}");
+Console.WriteLine($"Current Temp: {currentTemp}");
+Console.WriteLine($"Current Price: {currentPrice}");
